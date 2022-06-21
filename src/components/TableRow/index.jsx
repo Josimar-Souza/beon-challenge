fbd4808b8febbc5df4e691cf80c005300a052ef5
Josimar Souza <contact.josimarsouza@gmail.com>
2022-06-21 14:47:33 -0300

    Created TableRow component

---
+++ src/components/TableRow/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  TableRowStyle,
+  TableCell,
+} from './tableRowStyles';
+
+function TableRow() {
+  return (
+    <TableRowStyle>
+      <TableCell>Test</TableCell>
+    </TableRowStyle>
+  );
+}
+
+export default TableRow;
---
+++ src/components/TableRow/tableRowStyles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const TableRowStyle = styled.tr``;
+
+export const TableCell = styled.td``;
