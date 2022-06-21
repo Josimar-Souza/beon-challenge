bc1860483355b64d7112da0fcf5d79f312c25052
Josimar Souza <contact.josimarsouza@gmail.com>
2022-06-21 16:29:31 -0300

    Added filter section

--- src/components/SearchFilter/index.jsx
+++ src/components/SearchFilter/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import calendarIcon from '../../icons/calendar.svg';
 import {
   FilterContainer,
   FilterText,
+  YearFilterContainer,
+  FilterInput,
+  FilterIcon,
+  InputIconContainer,
 } from './searchFilterStyles';
 
 function SearchFilter() {
   return (
     <FilterContainer>
-      <FilterText>Filtrar ano da publicação:</FilterText>
+      <YearFilterContainer>
+        <FilterText>Filtrar ano da publicação:</FilterText>
+        <InputIconContainer>
+          <FilterInput type="number" />
+          <FilterIcon src={calendarIcon} alt="Icone de filtro por ano" />
+        </InputIconContainer>
+        <FilterText>até</FilterText>
+        <InputIconContainer>
+          <FilterInput type="number" />
+          <FilterIcon src={calendarIcon} alt="Icone de filtro por ano" />
+        </InputIconContainer>
+      </YearFilterContainer>
     </FilterContainer>
   );
 }
--- src/components/SearchFilter/searchFilterStyles.js
+++ src/components/SearchFilter/searchFilterStyles.js
@@ -11,5 +11,27 @@ export const FilterContainer = styled.div`
 `;
 
 export const FilterText = styled.p`
+  display: inline-block;
   font-size: 1vw;
+`;
+
+export const YearFilterContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  max-width: 40%;
+  width: 100%;
+`;
+
+export const FilterInput = styled.input`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+export const FilterIcon = styled.img`
+  margin-left: 5px;
+`;
+
+export const InputIconContainer = styled.div`
+  align-items: center;
+  display: flex;
 `;
