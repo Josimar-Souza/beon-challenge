138d284ca6e1b5a207984983d4a2c4720b0241a4
Josimar Souza <contact.josimarsouza@gmail.com>
2022-06-22 17:15:57 -0300

    Created a term search test

--- src/tests/MainPage/mainPage.test.jsx
+++ src/tests/MainPage/mainPage.test.jsx
@@ -2,13 +2,25 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
 import pages from '../../pages';
 import { booksAPI } from '../../pages/MainPage';
+import booksList from '../mocks/booksList';
 
 describe('Verifica a existência dos elementos do header', () => {
   beforeEach(() => {
-    jest.spyOn(booksAPI, 'getSeachedBooks').mockResolvedValue([]);
+    jest.spyOn(booksAPI, 'getSeachedBooks').mockImplementation(async (term) => {
+      if (term === '') {
+        return booksList;
+      }
+
+      const booksFound = booksList.filter((book) => (
+        book.title === term || book.author === term || book.language === term
+      ));
+
+      return booksFound;
+    });
   });
 
   it('Um título escrito "beon"', async () => {
@@ -36,5 +48,22 @@ describe('Verifica a existência dos elementos do header', () => {
 
     const searchInput = await screen.findByPlaceholderText('Busque livros pelo título, autor ou idioma');
     expect(searchInput).toBeInTheDocument();
+  });
+
+  it('Verifica se é possivel realizar uma busca pelo título', async () => {
+    await act(async () => {
+      renderWithRouter(<pages.MainPage />);
+    });
+
+    const searchInput = await screen.findByPlaceholderText('Busque livros pelo título, autor ou idioma');
+    const searchButton = await screen.findByRole('button', { name: 'Buscar' });
+
+    await act(async () => {
+      userEvent.type(searchInput, 'Test author 2');
+      userEvent.click(searchButton);
+    });
+
+    const bookTitle = await screen.findByRole('cell', { name: 'Test author 2' });
+    expect(bookTitle).toBeInTheDocument();
   });
 });
