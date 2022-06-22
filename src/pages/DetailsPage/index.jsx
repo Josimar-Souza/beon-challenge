4425618637b024a931c7a3e0882deb06cfb457c2
Josimar Souza <contact.josimarsouza@gmail.com>
2022-06-22 10:12:23 -0300

    Created details page base structure

---
+++ src/pages/DetailsPage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DetailsMainSection from './detailsPageStyle';
+
+function DetailsPage() {
+  return (
+    <DetailsMainSection>
+      <h1>Details Pàge</h1>
+    </DetailsMainSection>
+  );
+}
+
+export default DetailsPage;
