ac90a1018a59bfbf47d691890ed03e245f5e59af
Josimar Souza <contact.josimarsouza@gmail.com>
2022-06-22 10:42:29 -0300

    Added function to get the correct book

--- src/pages/DetailsPage/index.jsx
+++ src/pages/DetailsPage/index.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import DetailsMainSection from './detailsPageStyle';
+import BooksAPI from '../../api/booksAPI';
+
+const booksAPI = new BooksAPI('http://localhost:4000', 10000);
 
 function DetailsPage() {
+  const { title } = useParams();
+  const [book, setBook] = useState({});
+
+  useEffect(() => {
+    const getBook = async () => {
+      const fetchedBook = await booksAPI.getSeachedBooks(title);
+      setBook(fetchedBook);
+    };
+
+    getBook();
+  }, []);
+
   return (
     <DetailsMainSection>
       <h1>Details Page</h1>
